1f4a1c2478d69476b9c1ab5c3fdd1f73b01c74c9
Muhammed Ajmal <ajmalconsults@gmail.com>
2026-06-25 03:27:14 +0400

    fix: wrap sibling JSX elements in fragment in AppLayout

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -15,20 +15,22 @@ export default async function AppLayout({ children }: { children: React.ReactNod
   if (!user) redirect('/login')
 
   return (
-    <div className="flex h-dvh overflow-hidden">
-      <Sidebar />
+    <>
+      <div className="flex h-dvh overflow-hidden">
+        <Sidebar />
 
-      <div className="flex-1 flex flex-col min-w-0 overflow-hidden">
-        <Topbar email={user.email} />
+        <div className="flex-1 flex flex-col min-w-0 overflow-hidden">
+          <Topbar email={user.email} />
 
-        <main className="flex-1 overflow-y-auto pb-16 md:pb-0">
-          {children}
-        </main>
+          <main className="flex-1 overflow-y-auto pb-16 md:pb-0">
+            {children}
+          </main>
 
-        <BottomNav />
+          <BottomNav />
+        </div>
       </div>
-    </div>
-    <InstallPrompt />
-    <SWUpdateToast />
+      <InstallPrompt />
+      <SWUpdateToast />
+    </>
   )
 }
